2cf994259a8b46615cd69660a2a54912eef62281
Pedro Henrique <pedrohenriqueb366@gmail.com>
2023-11-08 11:48:33 -0300

    update soft skills

--- Javascript/assets/js/main.js
+++ Javascript/assets/js/main.js
@@ -1,17 +1,34 @@
 function updateProfileInfo(profileData){
+
     const photo = document.getElementById('profile.photo')
     photo.src = profileData.photo
     photo.alt = profileData.name
 
     const name = document.getElementById('profile.name')
-    const jop = document.getElementById('profile.job')
+    name.innerHTML = profileData.name
+
+    const job = document.getElementById('profile.job')
+    job.innerHTML = profileData.job
+
     const location = document.getElementById('profile.location')
+    location.innerHTML = profileData.location
+
     const phone = document.getElementById('profile.phone')
+    phone.innerHTML = profileData.phone
+
     const email = document.getElementById('profile.email')
+    email.innerHTML = profileData.email
+}
+
+function updateSoftSkills(profileData) {
+    const softSkills = document.getElementById('profile.skills.softSkills');
+
+    softSkills.innerHTML = profileData.skills.softSkills.map(skill => `<li>${skill}</li>`).join('')
 }
 
 
 (async () => {
     const profileData = await fetchProfileData()
     updateProfileInfo(profileData)
+    updateSoftSkills(profileData) 
 })();
